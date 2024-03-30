3d52aa123f2f58bf87befe49405e56e4f26c65cf
samuel-garciag-um-es <samuel.garciag@um.es>
2024-03-30 19:29:52 +0100

    3 versión

--- entregable1universidad.py
+++ entregable1universidad.py
@@ -1,23 +1,38 @@
 from enum import Enum
+from abc import ABCMeta, abstractmethod
 
 class Sexo(Enum):
         V = 1
         M = 2
 
-class Persona:
+class Persona(metaclass=ABCMeta):
     def __init__(self, nombre, dni, direccion, sexo):
         self.nombre = nombre
         self.dni = dni
         self.direccion = direccion
         self.sexo = sexo
 
+    @abstractmethod
     def __str__(self):
-        return f"Nombre {self.nombre}, DNI {self.dni}"
+        pass
+
+    # def __eq__(self, other):
+    #     if isinstance(other, Persona):
+    #         return self.dni == other.dni
+    #     return False
 
 class Asignatura:
     def __init__(self, nombre, codigo):
         self.nombre = nombre
         self.codigo = codigo
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Código: {self.codigo}"
+    
+    def __eq__(self, otro):
+        if isinstance(otro, Asignatura):
+            return self.codigo == otro.codigo
+        return False
 
 class Departamento(Enum):
         DIIC = 1
@@ -36,14 +51,28 @@ class MiembroDepartamento(Persona):
             self.departamento = nuevodepartamento
         else:
             raise TypeError("El departamento debe ser de la clase Departamento")
-        
+      
+    @abstractmethod
     def __str__(self):
-        return f"Nombre: {self.nombre}, DNI: {self.dni}, Dirección: {self.direccion}, Sexo: {self.sexo.name}, Departamento: {self.departamento.name}"
+        pass
+
+    def __eq__(self, other):
+        if isinstance(other, MiembroDepartamento):
+            return self.dni == other.dni
+        return False
 
 class Investigador(MiembroDepartamento):
     def __init__(self, nombre, dni, direccion, sexo, departamento, area_investigacion):
         MiembroDepartamento.__init__(self, nombre, dni, direccion, sexo, departamento)
         self.area_investigacion = area_investigacion
+    
+    def __eq__(self, other):
+        if isinstance(other, Investigador):
+            return self.dni == other.dni
+        return False
+    
+    def __str__(self):
+        return f"Nombre: {self.nombre}, DNI: {self.dni}, Dirección: {self.direccion}, Sexo: {self.sexo.name}, Departamento: {self.departamento.name}, Área: {self.area_investigacion}, Rol: Investigador"
 
 class Profesor(MiembroDepartamento):
     def __init__(self, nombre, dni, direccion, sexo, departamento, asignaturas):
@@ -54,28 +83,69 @@ class Profesor(MiembroDepartamento):
             if not isinstance(asignatura, Asignatura):
                 raise TypeError("Las asignaturas deben ser instancias de la clase Asignatura")
         self.asignaturas = asignaturas
-        
+
+    def __eq__(self, other):
+        if isinstance(other, Profesor):
+            return self.dni == other.dni
+        return False
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
 class Asociado(Profesor):
     def __init__(self, nombre, dni, direccion, sexo, departamento, asignaturas):
         Profesor.__init__(self, nombre, dni, direccion, sexo, departamento, asignaturas)
-        
+
+    def __eq__(self, other):
+        if isinstance(other, Asociado):
+            return self.dni == other.dni
+        return False
+
+    def __str__(self):
+        asignaturas_str = ["; ".join(str(asignatura) for asignatura in self.asignaturas)]
+        return f"Nombre: {self.nombre}, DNI: {self.dni}, Dirección: {self.direccion}, Sexo: {self.sexo.name}, Departamento: {self.departamento.name}, Asignaturas: {asignaturas_str}, Rol: Profesor asociado"
+            
 class Titular(Profesor, Investigador):                                      # hay que ver lo del departamento
     def __init__(self, nombre, dni, direccion, sexo, departamento, asignaturas, area_investigacion):
         Profesor.__init__(self, nombre, dni, direccion, sexo, departamento, asignaturas)
         Investigador.__init__(self, nombre, dni, direccion, sexo, departamento, area_investigacion)
 
-class Estudiante(Persona):
-    def __init__(self, nombre, dni, direccion, sexo):
-        Persona.__init__(self, nombre, dni, direccion, sexo)
+    def __eq__(self, other):
+        if isinstance(other, Titular):
+            return self.dni == other.dni
+        return False
+    
     def __str__(self):
-        return f"Nombre: {self.nombre}, Edad: {self.dni}, Direccion: {self.direccion}, Sexo: {self.sexo.name}"
+        asignaturas_str = ["; ".join(str(asignatura) for asignatura in self.asignaturas)]
+        return f"Nombre: {self.nombre}, DNI: {self.dni}, Dirección: {self.direccion}, Sexo: {self.sexo.name}, Departamento: {self.departamento.name}, Asignaturas: {asignaturas_str}, Área: {self.area_investigacion}, Rol: Profesor titular"
+
+
+class Estudiante(Persona):
+    def __init__(self, nombre, dni, direccion, sexo, asignaturas):
+        Persona.__init__(self, nombre, dni, direccion, sexo)
+        if not isinstance(asignaturas, list):       #permite pasar tanto 1 como n instancias de Asginatura
+            asignaturas = [asignaturas]
+        for asignatura in asignaturas:
+            if not isinstance(asignatura, Asignatura):
+                raise TypeError("Las asignaturas deben ser instancias de la clase Asignatura")
+        self.asignaturas = asignaturas
+
+    def __str__(self):
+        asignaturas_str = ["; ".join(str(asignatura) for asignatura in self.asignaturas)]
+        return f"Nombre: {self.nombre}, Edad: {self.dni}, Direccion: {self.direccion}, Sexo: {self.sexo.name}, asignaturas {asignaturas_str}, Rol: Estudiante"
+    
+    def __eq__(self, other):
+        if isinstance(other, Estudiante):
+            return self.dni == other.dni
+        return False
 
 class Universidad:
     def __init__(self, nombre, ubicacion):
         self.nombre = nombre
         self.ubicacion = ubicacion
         self.listaalumno = []
-        self.listamiembros = [] 
+        self.listamiembros = []
     
     def añadir_miembrodep(self, miembro):
         for i in self.listamiembros:
@@ -131,7 +201,55 @@ class Universidad:
         else:
             print("La lista de alumnos es: ")
             print('\n'.join(str(alumno) for alumno in self.listaalumno))
+    
+    def mostrar_asignaturas_profesor(self,persona=None):
+        if persona != None:
+            if persona.asignaturas == []:
+                print(f"{persona.nombre} no tiene asignaturas")
+            elif isinstance(persona, Profesor):
+                print(f"Lista de asignaturas del profesor {persona.nombre}: ", end="")
+                for i in range(len(persona.asignaturas) - 1):
+                    print(f"{persona.asignaturas[i]}; ", end="")
+                print(persona.asignaturas[-1])
+            else:
+                raise TypeError("El parámetro debe ser de la clase Profesor")
+        else:
+            for miembro in self.listamiembros:
+                if isinstance(miembro, Profesor):
+                    self.mostrar_asignaturas_profesor(miembro)
 
+    def mostrar_asignaturas_alumno(self,persona=None):
+        if persona != None:
+            if persona.asignaturas == []:
+                print(f"{persona.nombre} no tiene asignaturas")
+            elif isinstance(persona, Estudiante):
+                print(f"Lista de asignaturas del estudiante {persona.nombre}: ", end="")
+                for i in range(len(persona.asignaturas) - 1):
+                    print(f"{persona.asignaturas[i]}; ", end="")
+                print(persona.asignaturas[-1])
+            else:
+                raise TypeError("El parámetro debe ser de la clase Estudiante")
+        else:
+            for estudiante in self.listaalumno:
+                self.mostrar_asignaturas_alumno(estudiante)
+
+    def añadir_asignatura(self,profesor,asignatura):
+        if isinstance(profesor, Profesor):
+            if isinstance(asignatura, Asignatura):
+                profesor.asignaturas.append(asignatura)
+            else:
+                raise TypeError("Asignatura tiene que ser de la clase Asignatura")
+        else:
+            raise TypeError("Profesor tiene que ser de la clase Profesor")
+
+    def eliminar_asignatura(self,profesor,asignatura):
+        if isinstance(profesor, Profesor):
+            if isinstance(asignatura, Asignatura):
+                profesor.asignaturas.remove(asignatura)
+            else:
+                raise TypeError("Asignatura tiene que ser de la clase Asignatura")
+        else:
+            raise TypeError("Profesor tiene que ser de la clase Profesor")
 
 
 # umu = Universidad ("Universidad de Murcia", "Espinardo, Murcia")
@@ -150,16 +268,16 @@ class Universidad:
 
 umu = Universidad ("Universidad de Murcia", "Espinardo, Murcia")
 
-p1 = Estudiante("Lucas","3452566A", "Av del Campo 13",  Sexo.V)
-marcos = Estudiante("Marcos","6326725G", "Carril torremolina", Sexo.V)
-p3=Estudiante("Lucia","2398409P", "Calle abe",  Sexo.M)
+p1 = Estudiante("Lucas","3452566A", "Av del Campo 13",  Sexo.V, [Asignatura("1", 23), Asignatura("2",345)])
+marcos = Estudiante("Marcos","6326725G", "Carril torremolina", Sexo.V, Asignatura("ashas",234))
+p3=Estudiante("Lucia","2398409P", "Calle abe",  Sexo.M, Asignatura("ashas",234))
 umu.añadir_alumno (p1)
 umu.añadir_alumno (marcos)
 umu.añadir_alumno (p3)
 umu.mostrar_alumnos()
 
 p4=Titular("Fernando","2183293Ñ", "Carril lobo", Sexo.V ,Departamento.DIIC,Asignatura("ashas",234),"area1" )
-p5=Profesor("Pepe","3267836P", "Calle ñoro",  Sexo.V,Departamento.DITEC, Asignatura("1", 23))
+p5=Asociado("Pepe","3267836P", "Calle ñoro",  Sexo.V,Departamento.DITEC, [Asignatura("1", 23), Asignatura("2",345)])
 p6=Investigador("Carlota","3467387Y", "Calle radamel falcao", Sexo.M, Departamento.DIS,"area2")
 umu.añadir_miembrodep (p4)
 umu.añadir_miembrodep (p5)
@@ -168,11 +286,16 @@ umu.mostrar_miembros()
 
 umu.eliminar_alumno(marcos)   #esto no va porque la llista tiene objetosy no lo identifica 
 umu.eliminar_miembrodep(p6)
-p7=Estudiante("Marlucas","322346U", "Carril piruleta",  Sexo.V)
+p7=Estudiante("Marlucas","322346U", "Carril piruleta",  Sexo.V, Asignatura("ds",1231))
 umu.eliminar_alumno(p7)
-umu.eliminar_miembrodep(MiembroDepartamento("Lolalolita","438I", "Calle bartolo", Sexo.V, Departamento.DIS))
+umu.eliminar_miembrodep(p7)
 
 umu.cambio_departamento(p4, Departamento.DIS)     #esto no lo actualiza
 
 umu.mostrar_alumnos()
 umu.mostrar_miembros()
+umu.mostrar_asignaturas_profesor(p5)
+umu.eliminar_asignatura(p5, Asignatura("1", 23))
+umu.mostrar_asignaturas_profesor(p5)
+umu.mostrar_asignaturas_profesor()
+umu.mostrar_asignaturas_alumno()
